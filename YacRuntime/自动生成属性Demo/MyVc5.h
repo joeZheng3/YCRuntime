6e7004b08db29365cfb977bc8fbc8e464dd5ad58
Nevergiveupchit <460446478@qq.com>
2017-04-19 00:47:39 +0800

    Second Commit
    Add some classes and methods

---
+++ YacRuntime/自动生成属性Demo/MyVc5.h
@@ -0,0 +1,13 @@
+//
+//  MyVc5.h
+//  YacRuntime
+//
+//  Created by ChangWingchit on 2017/4/18.
+//  Copyright © 2017年 chit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyVc5 : UIViewController
+
+@end
